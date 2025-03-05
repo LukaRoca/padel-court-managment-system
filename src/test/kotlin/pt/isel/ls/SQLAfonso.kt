7b59c72faf0af06933c0df4b9eb289d4a2e743c9
AfonsoSantos44 <reiafonso07@gmail.com>
2025-03-05 16:10:56 +0000

    changed my sql tests

--- src/test/kotlin/pt/isel/ls/SQLAfonso.kt
+++ src/test/kotlin/pt/isel/ls/SQLAfonso.kt
@@ -18,8 +18,8 @@ class DataBaseTestsAfonso {
         val connection = connect()
         val sql = "INSERT INTO students (number, name, course) VALUES (?, ?, ?)"
         connection.prepareStatement(sql).use { stmt ->
-            stmt.setInt(1, 51820)
-            stmt.setString(2, "Luka")
+            stmt.setInt(1, 50484)
+            stmt.setString(2, "Afonso")
             stmt.setInt(3, getCourseId("LEIC", connection))
             val rowsInserted = stmt.executeUpdate()
             assertTrue(rowsInserted > 0, "Falha ao inserir o aluno")
@@ -34,7 +34,7 @@ class DataBaseTestsAfonso {
             val rs: ResultSet = stmt.executeQuery()
             var found = false
             while (rs.next()) {
-                if (rs.getInt("number") == 51820) {
+                if (rs.getInt("number") == 50484) {
                     found = true
                     break
                 }
@@ -48,8 +48,8 @@ class DataBaseTestsAfonso {
         val connection = connect()
         val sql = "UPDATE students SET name = ? WHERE number = ?"
         connection.prepareStatement(sql).use { stmt ->
-            stmt.setString(1, "Lukao")
-            stmt.setInt(2, 51820)
+            stmt.setString(1, "Afonso")
+            stmt.setInt(2, 50484)
             val rowsUpdated = stmt.executeUpdate()
             assertTrue(rowsUpdated > 0, "Falha ao atualizar o aluno")
         }
@@ -60,7 +60,7 @@ class DataBaseTestsAfonso {
         val connection = connect()
         val sql = "DELETE FROM students WHERE number = ?"
         connection.prepareStatement(sql).use { stmt ->
-            stmt.setInt(1, 51820)
+            stmt.setInt(1, 50484)
             val rowsDeleted = stmt.executeUpdate()
             assertTrue(rowsDeleted > 0, "Falha ao deletar o aluno")
         }
