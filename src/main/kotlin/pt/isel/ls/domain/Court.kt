e207a063490083ae040a314531ab391f5bc2823d
Luka Roca <lukastroca@gmail.com>
2025-03-12 16:01:47 +0000

    Sql testes

--- src/main/kotlin/pt/isel/ls/domain/Court.kt
+++ src/main/kotlin/pt/isel/ls/domain/Court.kt
@@ -1,2 +1,18 @@
 package pt.isel.ls.domain
 
+data class Id (val id : Int)
+data class Name (val name : String)
+data class Owner (val name : Name)
+
+data class Court(
+    val id : Id,
+    val name : Name,
+    val owner : Owner,
+) {
+    init {
+        require(name.name.isNotBlank()) { "Name must not be empty" }
+        require(owner.name.name.isNotBlank()) { "Owner must not be empty" }
+    }
+}
+
+
