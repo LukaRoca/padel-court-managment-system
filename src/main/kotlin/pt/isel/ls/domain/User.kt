1479b6eb4b5a68d4e7c940152677933a4c3dad0b
Luka Roca <lukastroca@gmail.com>
2025-03-12 16:29:49 +0000

    User e testes user

--- src/main/kotlin/pt/isel/ls/domain/User.kt
+++ src/main/kotlin/pt/isel/ls/domain/User.kt
@@ -1,2 +1,19 @@
 package pt.isel.ls.domain
 
+data class Uid (val id : Int) {init {
+    require(id > 0) { "id must be positive" }
+}}
+data class UserName (val name : String)
+data class Email (val email : String) {init {
+    require("@" in email) { "Email must have @ in it"  }
+}}
+
+data class User(
+    val id : Uid,
+    val user : UserName,
+    val email : Email,
+){
+    init {
+        require(user.name.isNotBlank()) { "User must have a name" }
+    }
+}
