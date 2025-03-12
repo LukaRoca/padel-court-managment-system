7de1551a1de0542c5730ab670d016a04088dca4d
AfonsoSantos44 <reiafonso07@gmail.com>
2025-03-12 16:10:53 +0000

    user class and added new file to store new data classes

--- src/main/kotlin/pt/isel/ls/domain/Court.kt
+++ src/main/kotlin/pt/isel/ls/domain/Court.kt
@@ -1,9 +1,5 @@
 package pt.isel.ls.domain
 
-data class Id (val id : Int)
-data class Name (val name : String)
-data class Owner (val name : Name)
-
 data class Court(
     val id : Id,
     val name : Name,
--- src/main/kotlin/pt/isel/ls/domain/User.kt
+++ src/main/kotlin/pt/isel/ls/domain/User.kt
@@ -1,20 +1,5 @@
 package pt.isel.ls.domain
 
-data class Name( val value : String)
-
-data class Id( val value : Int){
-    init {
-        if(value <= 0)
-            throw IllegalArgumentException("Invalid id")
-    }
-}
-data class Email( val value : String){
-    init {
-        if(!value.contains("@"))
-            throw IllegalArgumentException("Invalid email")
-    }
-}
-
 data class User(
     val uid : Id,
     val name: Name,
