aa4990d186ecbc66f3e2d1278b8fb6865fc4ac01
AfonsoSantos44 <reiafonso07@gmail.com>
2025-03-12 16:07:30 +0000

    user class

--- src/main/kotlin/pt/isel/ls/domain/User.kt
+++ src/main/kotlin/pt/isel/ls/domain/User.kt
@@ -1,2 +1,22 @@
 package pt.isel.ls.domain
 
+data class Name( val value : String)
+
+data class Id( val value : Int){
+    init {
+        if(value <= 0)
+            throw IllegalArgumentException("Invalid id")
+    }
+}
+data class Email( val value : String){
+    init {
+        if(!value.contains("@"))
+            throw IllegalArgumentException("Invalid email")
+    }
+}
+
+data class User(
+    val uid : Id,
+    val name: Name,
+    val email : Email,
+)
--- src/main/sql/createSchema.sql
+++ src/main/sql/createSchema.sql
@@ -1,26 +1,6 @@
 
-/*
-Apagar depois visto que ja n vai ser necessario
-
-drop table if exists students;
-drop table if exists courses;
-
-create table courses (
-  cid serial primary key,
-  name varchar(80)
-);
-
-create table students (
-  number int primary key,
-  name varchar(80),
-  course int references courses(cid)
-);
-
- */
-
-
 create table "user" (
-    uid uuid unique primary key,
+    uid serial primary key,
     name varchar(255) not null,
     email varchar(255) unique not null
 );
