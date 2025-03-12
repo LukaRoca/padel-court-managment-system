73bb65daa33fb22237f5cda336611c8f24fe1bd6
Luka Roca <lukastroca@gmail.com>
2025-03-12 14:14:11 +0000

    Sql testes

--- src/main/sql/addData.sql
+++ src/main/sql/addData.sql
@@ -5,6 +5,6 @@ insert into courses(name) values ('LEIRT');
 
 
 insert into students(course, number, name) values (1, 12345, 'Alice');
-insert into students(course, number, name) select cid as course, 12346 as number, 'Bob' as name from courses where name = 'LEIC';
+insert into students(course, number, name) select cid as course, 51820 as number, 'Luka' as name from courses where name = 'LEIC';
 insert into students(course, number, name) select cid as course, 50484 as number, 'Afonso' as name from courses where name = 'LEIC';
 insert into students(course, number, name) select cid as course, 51690 as number, 'Bernardo' as name from courses where name = 'LEIC';
