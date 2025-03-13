36b9c12cac834f59adaaaff106ffe22a9b1037b0
Luka Roca <lukastroca@gmail.com>
2025-03-13 15:58:59 +0000

    Club

--- src/main/kotlin/pt/isel/ls/storage/IStorage.kt
+++ src/main/kotlin/pt/isel/ls/storage/IStorage.kt
@@ -1,9 +1,11 @@
 package pt.isel.ls.storage
 
+import pt.isel.ls.domain.Club
 import pt.isel.ls.domain.User
 
 interface IStorage{
 
     fun getUsers(): List<User>
     fun getUserById(userId: Int): User?
+    fun getClubs() : List<Club>
 }
