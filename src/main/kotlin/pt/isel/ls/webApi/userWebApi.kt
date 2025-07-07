f99eea4a1419bfd28c95a43aff1d04666b4419de
Luka Roca <lukastroca@gmail.com>
2025-07-07 17:26:28 +0100

    Atualizações

--- src/main/kotlin/pt/isel/ls/webApi/userWebApi.kt
+++ src/main/kotlin/pt/isel/ls/webApi/userWebApi.kt
@@ -3,72 +3,48 @@ package pt.isel.ls.webApi
 import kotlinx.serialization.json.Json
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.CREATED
+import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.path
-import org.slf4j.LoggerFactory
-import pt.isel.ls.utils.Email
+import pt.isel.ls.DEFAULT_LIMIT
+import pt.isel.ls.DEFAULT_SKIP
 import pt.isel.ls.utils.Id
-import pt.isel.ls.utils.Name
-import pt.isel.ls.utils.Password
 import pt.isel.ls.utils.isNotNegative
 import pt.isel.ls.utils.validateInt
-import pt.isel.ls.webApi.dto.UserDetails
+import pt.isel.ls.webApi.models.user.UserCreate
+import pt.isel.ls.webApi.models.user.UserLogin
+import pt.isel.ls.webApi.models.user.UserSearch
 import pt.isel.ls.webServices.UserServices
-import pt.isel.ls.webApi.dto.UserOutput
-import pt.isel.ls.webApi.dto.UserInput
-import pt.isel.ls.webApi.dto.UserLoginInput
 
-class UserWebApi(private val userServices: UserServices) : WebApiExceptions() {
-    val logger = LoggerFactory.getLogger("pt.isel.ls.webApi.routes.user.UserRoute")
-
-    fun logRequest(request: Request) {
-        logger.info(
-            "incoming request: method={}, uri={}, content-type={} accept={}",
-            request.method,
-            request.uri,
-            request.header("content-type"),
-            request.header("accept"),
-        )
-    }
-    fun getUserById(request: Request): Response = useWithException {
-        logRequest(request)
-        val userId = request.path("id")?.toIntOrNull()
-            ?: throw IllegalArgumentException("Invalid user ID")
-        val user = userServices.getUserById(Id(userId)) ?: throw NoSuchElementException()
-        Response(OK).json(UserDetails(user.uid.id, user.name.name, user.email.value, user.token.token))
-    }
-
-
-    fun createUser(request: Request): Response = useWithException {
-        logRequest(request)
-        val response = Json.decodeFromString<UserInput>(request.bodyString())
-        try {
-            val user = userServices.createUser(Name(response.name), Email(response.email), Password(response.password))
-            Response(CREATED).json(UserOutput(user.uid.id, user.token.token))
-        } catch (e: Exception) {
-            throw e
+class userWebApi(val services: UserServices) : APISchema() {
+    fun createUser(request: Request): Response =
+        request.useWithExceptionNoToken {
+            val input = Json.decodeFromString<UserCreate>(request.bodyString())
+            Response(Status.CREATED)
+                .json(services.createUser(input))
         }
-    }
 
-    fun getAllUsers(request: Request): Response = useWithException {
-        logRequest(request)
-        val limit = request.query("limit")?.toInt().validateInt { it.isNotNegative() }
-        val skip  = request.query("skip")?.toInt().validateInt { it.isNotNegative() }
-
-        val paginatedResult = userServices.getAllUsers(limit, skip)
-
-        Response(OK).json(paginatedResult)
-    }
-    fun loginUser(request: Request): Response = useWithException {
-        logRequest(request)
-        val loginData = Json.decodeFromString<UserLoginInput>(request.bodyString())
-
-        try {
-            val user = userServices.loginUser(Email(loginData.email), Password(loginData.password))
-            Response(OK).json(UserOutput(user.uid.id, user.token.token))
-        } catch (e: Exception) {
-            throw e
+    fun getUser(request: Request): Response =
+        request.useWithException { token ->
+            val userId = request.path("id")?.toInt().validateInt { it.isNotNegative() }
+            Response(Status.OK)
+                .json(
+                    services.getUser(Id(userId), token),
+                )
         }
-    }
+
+    fun getAllUsers(request: Request): Response =
+        request.useWithException { token ->
+            val value = Json.decodeFromString<UserSearch>(request.bodyString())
+            val skip = request.query("skip")?.toInt().validateInt(DEFAULT_SKIP) { it.isNotNegative() }
+            val limit = request.query("limit")?.toInt().validateInt(DEFAULT_LIMIT) { it.isNotNegative() }
+            Response(OK).json(services.getAllUsers(value, token,skip,limit))
+        }
+
+    fun loginUser(request: Request): Response =
+        request.useWithExceptionNoToken {
+            val input = Json.decodeFromString<UserLogin>(request.bodyString())
+            Response(Status.OK)
+                .json(services.loginUser(input))
+        }
 }
